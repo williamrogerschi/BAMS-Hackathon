d3ff3085f8e4ec7268dbb6413df66942b4a78ad9
annemarielewis <a.m.lewisfilms@gmail.com>
2023-10-11 01:48:48 -0600

    21 demos

--- db/index.js
+++ db/index.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/plantsDatabase')
-    .then(() => {
-        console.log('Successfully connected to MongoDB.')
-    })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+  .connect("mongodb://127.0.0.1:27017/demoBikeDatabase")
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  })
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
 
+const db = mongoose.connection;
 
-const db = mongoose.connection
-
-module.exports = db
+module.exports = db;
