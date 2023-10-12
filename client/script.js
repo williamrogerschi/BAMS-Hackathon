9cf2e0ffc186fa3c28b18686d770e7975c4ea469
annemarielewis <a.m.lewisfilms@gmail.com>
2023-10-12 10:03:16 -0600

    cart not working

--- client/script.js
+++ client/script.js
@@ -2,28 +2,82 @@ const demoPic = document.querySelector(".grid-image");
 
 // MAIN function for API calls
 
-window.addEventListener('load', async (event) => {
-    event.preventDefault()
-        let demoAll = await axios.get(`http://localhost:3001/demos`)
-        let demoBikes = demoAll.data
-        let gridHTML = document.getElementById("demo-bikes-container")
-        console.log(demoBikes)
-            for(let i = 0; i < demoBikes.length; i++) {
-                gridHTML.innerHTML += `<a class="grid-item" id="box${i}">` //also added it up here - we can see once the button actually functions if it will let us click both while the whole a div is a link.
-               
-                let gridItem = document.querySelector(`#box${i}`)
+window.addEventListener("load", async (event) => {
+  event.preventDefault();
+  let demoAll = await axios.get(`http://localhost:3001/demos`);
+  let demoBikes = demoAll.data;
+  let gridHTML = document.getElementById("demo-bikes-container");
+  console.log(demoBikes);
+  for (let i = 0; i < demoBikes.length; i++) {
+    gridHTML.innerHTML += `<a class="grid-item" id="box${i}">`; //also added it up here - we can see once the button actually functions if it will let us click both while the whole a div is a link.
 
-                let htmlString = new String()
-                
-                // htmlString += `<a class="demo-image" target="_blank" href="${demoBikes[i].url}"><img class="grid-image" src="${demoBikes[i].imageUrl}"></a>\n` //possibly can add the link to the ex site in the image?
-                htmlString += `<img class="grid-image" src="${demoBikes[i].imageUrl}">\n` //possibly can add the link to the ex site in the image?
-                htmlString += `<div class="grid-brand">${demoBikes[i].brand}</div>\n`
-                htmlString += `<div class="grid-model">${demoBikes[i].model}</div>\n`
-                htmlString += `<div class="grid-price">${demoBikes[i].priceUSD}</div>\n`
-                htmlString += `<div class="cart-btn" id="${demoBikes[i]._id}">Add to Cart</div>\n`
+    let gridItem = document.querySelector(`#box${i}`);
 
+    let htmlString = new String();
 
-                gridItem.innerHTML += htmlString
-            }
-})
+    // htmlString += `<a class="demo-image" target="_blank" href="${demoBikes[i].url}"><img class="grid-image" src="${demoBikes[i].imageUrl}"></a>\n` //possibly can add the link to the ex site in the image?
+    htmlString += `<img class="grid-image" src="${demoBikes[i].imageUrl}">\n`; //possibly can add the link to the ex site in the image?
+    htmlString += `<div class="grid-brand">${demoBikes[i].brand}</div>\n`;
+    htmlString += `<div class="grid-model">${demoBikes[i].model}</div>\n`;
+    htmlString += `<div class="grid-price">${demoBikes[i].priceUSD}</div>\n`;
+    htmlString += `<button class="cart-btn" id="${demoBikes[i]._id}">Add to Cart</button>\n`;
 
+    gridItem.innerHTML = htmlString;
+  }
+
+  //query to get the html elements
+  const addToCartButtons = document.querySelectorAll(".cart-btn");
+  const Cart = document.getElementById("cart");
+
+  const shoppingCart = [];
+  console.log(shoppingCart);
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log("button working");
+      const itemId = button.getAttribute("id"); //this needs to be the items id in the database--> each button needs the html id of the data's id
+      //console.log(itemID);
+      addToCart(itemId);
+      console.log(shoppingCart);
+    });
+  });
+  console.log(shoppingCart);
+
+  async function addToCart(itemId) {
+    console.log(itemId);
+    //const item = document.getElementById("itemId");
+    try {
+      const object = await axios.GET("/demos/:${itemId}");
+      const itemData = response.data
+      const clonedItem = object.cloneNode(true);
+      const htmlShoppingCart = document.getElementById("cart");
+      shoppingCart.push(clonedItem);
+      htmlShoppingCart.appendChild(clonedItem);
+    } catch (error) {
+      alert("An error occurred");
+      console.error(error);
+    }
+  }
+});
+
+//     try {
+//       // Send a POST request to add the item to the shopping cart
+//       const response = await axios.post(`/cart/${itemId}`);
+//       //CHECK^
+
+//       if (response.status === 200) {
+//         const item = document.getElementById("itemId");
+//         const clonedItem = item.cloneNode(true);
+//         const htmlShoppingCart = document.getElementById("cart");
+//         shoppingCart.push(clonedItem);
+//         htmlShoppingCart.appendChild(clonedItem);
+//       } else if (response.status === 404) {
+//         alert("Item not found");
+//       } else {
+//         alert("An error occurred");
+//       }
+//     } catch (error) {
+//       alert("An error occurred");
+//       console.error(error);
+//     }
+//   }
+// });
--- server.js
+++ server.js
@@ -9,13 +9,20 @@ const demoController = require("./controllers/demoController.js");
 
 //IMPORTS - Bike Config
 
-const { Brand, FrameConfig, GroupsetConfig, WheelConfig, TireConfig, KitConfig } = require(`./models/configIndex.js`)
+const {
+  Brand,
+  FrameConfig,
+  GroupsetConfig,
+  WheelConfig,
+  TireConfig,
+  KitConfig,
+} = require(`./models/configIndex.js`);
 
-const brandConfigController = require(`./controllers/brandsConfigController.js`)
-const frameConfigController = require(`./controllers/frameConfigController.js`)
+const brandConfigController = require(`./controllers/brandsConfigController.js`);
+const frameConfigController = require(`./controllers/frameConfigController.js`);
 
 //IMPORTS - Cart
-const cartController = require(`./controllers/cartController.js`)
+const cartController = require(`./controllers/cartController.js`);
 
 const PORT = process.env.PORT || 3001;
 
@@ -34,8 +41,6 @@ app.get("/demos", demoController.getAllDemos);
 //get one demo by ID:
 app.get("/demos/:id", demoController.getOneDemo);
 
-
-
 //CRUD - Brands
 app.get(`/brands`, brandConfigController.getAll);
 app.get(`/brands/:id`, brandConfigController.getByID);
@@ -47,27 +52,23 @@ app.get(`/bike-builder/frames`, frameConfigController.getAll);
 //CRUD - Shopping Cart
 //app.get('/cart', cartController.getCart)
 
-
 // Get the contents of the shopping cart
 app.get("/cart", (req, res) => {
   res.json(shoppingCart);
 });
 
 // Add an item to the shopping cart array
-app.post('/cart/:id', async (req, res) => {
+app.post("/cart/:id", async (req, res) => {
   const { id } = req.params;
-  const item = await DemoModel.findOne({ id }); // Retrieve item details from the database
+  const item = await DemoModel.findOne(id); // Retrieve item details from the database
 
   if (item) {
-  
     shoppingCart.push(item);
-    res.status(200).send('Item added to the shopping cart');
+    res.status(200).send("Item added to the shopping cart");
   } else {
-    res.status(404).send('Item not found');
+    res.status(404).send("Item not found");
   }
 });
-
-
 
 //listening
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
